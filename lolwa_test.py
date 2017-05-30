cf092764669e743b355a98a648e51bcaaa4fa8d1
Trolldemort <tarunraheja1234@gmail.com>
2017-05-30 12:05:23 +0530

    Saved to file called Response.txt. Still have no way of mapping sentences to their responses.

---
+++ lolwa_test.py
@@ -0,0 +1,4 @@
+content = open('mussolini_input.txt')
+content_str = content.read()
+print(content_str)
+content_str = content_str.split('\n')
--- openie_first.py
+++ openie_first.py
@@ -3,9 +3,12 @@ import nltk
 from nltk.tree import Tree
 ROOT = 'ROOT'
 if __name__ == '__main__':
+    content = open('mussolini_input.txt')
+    content_str = content.read()
+    #print(content_str)
     nlp = StanfordCoreNLP('http://10.4.100.141:9000')
     text = (
-        'They were dancers.  I am a dancer.')
+        content_str)
     output = nlp.annotate(text, properties={
         'annotators': 'tokenize,ssplit,pos,depparse,parse,openie',
         'outputFormat': 'json'
@@ -39,15 +42,20 @@ if __name__ == '__main__':
     #dict1 = check_inheritance(output["sentences"][0]["openie"][0])
     #print(dict1)            
     
+    solution = []
     for sentence in output['sentences']:
         for op in sentence["openie"]:
             dict1 = check_inheritance(op);
             print(dict1)
+            solution.append(dict1)
                     
-                
-                
-    
-            
+    content_str = content_str.split('\n')
+    #print(len(content_str))
+    #print(len(solution))
+    file = open("relations.txt","w")
+    for i in range(len(solution)):
+        file.write(str(solution[i]))
+        file.write("\n")
 
 
     
